08593c9b09dc6331ef67f8a460f0bf0e0ff67f27
Leah Grim <grimchelate@gmail.com>
2023-04-20 15:22:58 -0500

    created protected route for notepad landing page

--- src/components/NotepadLandingPage/NotepadLandingPage.jsx
+++ src/components/NotepadLandingPage/NotepadLandingPage.jsx
@@ -6,14 +6,28 @@ import './NotepadLandingPage.css';
 
 function NotepadLandingPage() {
 
- //Hooks 
+ // Hooks 
     const history = useHistory();
     const Dispatch = useDispatch();
-
+// State Variables 
+    let [newDateAdded, setNewDateAdded] = useState('');
   return (
     <>
     <div className="container">
-
+    <div className= "InputAndBtn">
+          <label> Enter The Date Here 
+          <input
+            required
+            type="date"
+            name= "dateEntry"
+            id="dateEntry"
+            value={newDateAdded}
+            onChange={(e) => {
+              setNewDateAdded(e.target.value);
+            }}
+          />
+          </label>
+          </div>
     </div>
     </>
   );
