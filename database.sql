839a1347b50c07896867f483b0fa0ff0d61b8921
Chris Agbolade <yemluck@gmail.com>
2023-04-24 11:27:25 -0500

    created tables for notepad, todo pad and joke pad

--- database.sql
+++ database.sql
@@ -8,3 +8,26 @@ CREATE TABLE "user" (
     "username" VARCHAR (80) UNIQUE NOT NULL,
     "password" VARCHAR (1000) NOT NULL
 );
+
+CREATE TABLE "notepad" (
+    "id" SERIAL PRIMARY KEY,
+    "date" DATE NOT NULL,
+    "header" VARCHAR (100) NOT NULL,
+    "description" VARCHAR (1000) NOT NULL,
+    "user_id" INT REFERENCES "user"(id)
+);
+
+CREATE TABLE "todo_pad" (
+    "id" SERIAL PRIMARY KEY,
+    "date_created" DATE NOT NULL,
+    "task" VARCHAR (255) NOT NULL,
+    "status" INT NOT NULL,
+    "user_id" INT REFERENCES "user"(id)
+);
+
+CREATE TABLE "joke_pad" (
+    "id" SERIAL PRIMARY KEY,
+    "joke" VARCHAR (1000) NOT NULL,
+    "answer" VARCHAR (500) NOT NULL,
+    "rating" INT
+);
