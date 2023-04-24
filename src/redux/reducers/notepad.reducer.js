39cbffb4652df7a343e609e6362a6c9720a3a112
Leah Grim <grimchelate@gmail.com>
2023-04-24 16:27:54 -0500

    added notepadReducer

--- src/redux/reducers/notepad.reducer.js
+++ src/redux/reducers/notepad.reducer.js
@@ -1,12 +1,21 @@
 import { combineReducers } from 'redux';
 
 
+const notepadReducer = (state=[], action) => {
+    switch(action.type) {
+        case 'SET_NOTES': 
+            return action.payload
+        default:
+            return state;
+    }
+  }; // End resultsList reducer
+
 
 
 
 
 
 export default combineReducers({
-   
+   notepadReducer
   });
   
