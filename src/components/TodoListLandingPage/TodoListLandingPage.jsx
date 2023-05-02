356a4b9de102b03d7b4608e06835237c787720f2
Leah Grim <grimchelate@gmail.com>
2023-05-02 11:33:06 -0500

    established TodoList Landing page framework

---
+++ src/components/TodoListLandingPage/TodoListLandingPage.jsx
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from "react";
+import { useHistory, Link } from 'react-router-dom';
+import { useDispatch, useSelector } from "react-redux";
+import './TodoListLandingPage.css';
+
+
+function TodoListLandingPage() {
+
+ // Hooks 
+    const dispatch = useDispatch();
+// Reducers 
+   
+// State Variables 
+    let [newDateAdded, setNewDateAdded] = useState('');
+    let [description, setDescription] = useState('');
+    let [header, setHeader] = useState('');
+
+// Create notepad and descriptions, send to reducer via dispatch
+let ToDoToAdd = 
+{
+    date: newDateAdded,
+    header: header,
+    description: description
+}
+    
+// Page Load Functions:
+    useEffect(()=> {
+        dispatch({
+        type: 'FETCH_TODOS'
+        });
+    }, [])
+
+// Button Functions 
+    // function to send noteToAdd to saga 
+
+
+    // function to clear inputs after client has entered data
+    function handleClear(){
+        setNewDateAdded('');
+        setDescription('');
+        setHeader('');
+    }
+
+
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default TodoListLandingPage;
