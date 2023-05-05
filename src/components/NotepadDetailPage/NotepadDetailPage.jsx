4d81d585300657c4a00af51c2c0cd9177e828dfd
Leah Grim <grimchelate@gmail.com>
2023-05-04 21:57:13 -0500

    navigation is working properly

--- src/components/NotepadDetailPage/NotepadDetailPage.jsx
+++ src/components/NotepadDetailPage/NotepadDetailPage.jsx
@@ -1,13 +1,16 @@
 import React, { useState, useEffect } from "react";
 import { useHistory, Link } from 'react-router-dom';
 import { useDispatch, useSelector } from "react-redux";
-
+import { Link, useParams} from 'react-router-dom';
 import './NotepadDetailPage.css';
 
 
 function NotepadDetailPage() {
     //Hooks:
-  
+    const params = useParams();
+    const dispatch = useDispatch()
+
+
   
   
     return (
