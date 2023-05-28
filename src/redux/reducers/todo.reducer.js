7d9cdd76001fd0d9fc7359b77836de05721d22ff
Leah Grim <grimchelate@gmail.com>
2023-05-28 12:14:52 -0500

    reworded my notes

--- src/redux/reducers/todo.reducer.js
+++ src/redux/reducers/todo.reducer.js
@@ -1,4 +1,4 @@
-//reducer that will hold the data that is sent from GET request triggered at "FETCH_NOTES" on todolist container page
+//reducer that will hold the data that is sent from GET request triggered at "FETCH_TODO" on todolist container page
 const todoList = (state=[], action) => {
     switch(action.type) {
         case 'SET_TODO_LIST': 
