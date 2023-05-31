00a1d0edcd3235d65ca9fa784890b57f71ccb901
Chris Agbolade <yemluck@gmail.com>
2023-05-30 21:28:35 -0500

    css styling for memePad

--- src/components/MemeGeneratorLandingPage/MemeGeneratorLandingPage.jsx
+++ src/components/MemeGeneratorLandingPage/MemeGeneratorLandingPage.jsx
@@ -5,11 +5,25 @@ import './MemeGeneratorLandingPage.css'
 
 function MemeGeneratorLandingPage () {
 
+  const history = useHistory();
+
+
   return (
-    <>
-      <h2> Meme Generator Landing Page</h2>
+    <center>
+      <h2> Welcome to MemePad, ease your stress with top tier meme entertainment</h2>
+      <div className='gridParent'>
+        <div className='gridChild image'>
+          <img className='imageDiv' src='' alt='Meme here' height='500px' width='500px' />
+        </div>
+        <div className='gridChild button'>
+          <button className='btn1'>Generate New Meme</button><br></br>
+          <button className='btn2'>Save Meme</button><br></br>
+          <button className='btn3' onClick={() => history.push('/favoriteMeme')}> Meme History</button><br></br>
+        </div>
+      </div><br></br>
+      <br></br>
       <Link to='/user'><button> User Homepage</button></Link>
-    </>
+    </center>
   )
 }
 
